4490195a2abc2429f68ff54109c29becd7da7a1f
OliveiraNadia <n.nadia.oliveira7@gmail.com>
2016-04-16 01:04:23 -0300

    Create conexap.php

---
+++ model/conexap.php
@@ -0,0 +1,11 @@
+<?php
+function conexao ( $host, $user, $pass, $bd,$cn)
+{
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$bd = '';
+ 
+$cn = mysqli_connect($host, $user, $pass, $bd) or die ("falha na conexao");
+    
+}
