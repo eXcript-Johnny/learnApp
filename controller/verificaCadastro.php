7f350011249f895a32680ab9421d26ea795a8ce0
OliveiraNadia <n.nadia.oliveira7@gmail.com>
2016-04-17 22:31:38 -0300

    Update and rename verificaLogin.php to verificaCadastro.php

---
+++ controller/verificaCadastro.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<?php require_once ('../model/conexao.php')  ?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php 
+      
+		$nome = $_POST['nome'];
+        $email =$_POST['email'];
+        $telefone =$_POST['telefone'];
+        $senha =$_POST['senha'];
+        
+        $sql = mysqli_query("insert into usuarios (nome, email, telefone,senha)
+        values ('$nome','$email','$telefone', '$senha')");
+        
+     header("location: ../view/index.php")	
+        
+       
+	
+	
+	
+	
+        ?>
+    </body>
+</html>
