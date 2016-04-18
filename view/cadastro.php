d0e981b5f2ec2b3238f964426c12ad8f24fc687e
OliveiraNadia <n.nadia.oliveira7@gmail.com>
2016-04-17 22:34:01 -0300

    Update cadastro.php

--- view/cadastro.php
+++ view/cadastro.php
@@ -1,26 +1,36 @@
+<?php require_once ('../model/conexao.php') ?>
+
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Cadastro</title>
     
-        <form action="reservas.php" method="POST">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title> Cadastro </title>
+	<link rel="stylesheet" href="css/estilo.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+<meta http-equiv="Content-Type" content="text/html" charset=utf-8" />
+<title>Cadastro</title>
+</head>
+<body>
  
-        <tr>
-             <td class="nome">Nome :*</td><td><input type="text" name="nome"/></td>      
-         </tr> </br>
-         
-		<tr>
-              <td class="email">Email:*</td><td><input type="text" name="email"/></td>
-        </tr></br>
+    <div class="container">
+        <form action=../controller/verificaCadastro.php method="POST" accept-charset="utf-8" class="form-login">
+        <h2 class="form-cadastro-heading"> Cadastre-se </h2>
         
-		<tr>
-              <td class="telefone">Telefone:</td><td><input type="text" name="telefone"/></td>
-        </tr></br>
-		
-         <tr>
-             <td class="definirSenha">Definir Senha:*</td><td><input type="text" name="definirSenha"/></td>
-         </tr></br>
-	         
+     
+                <label class="nome">Nome: </label>
+                <input type="nome" id="nome" class="form-control" placeholder="nome" required autofocus>
+                
+                 <label class="email">Email:</label>
+                <input type="email" id="email" class="form-control" placeholder="email" required>
+                
+                <label class="telefone">Telefone:</label>
+                <input type="telefone" id="telefone" class="form-control" placeholder="telefone" required>
+                
+                <label for="inputPassword">Digite sua senha</label>
+		<input type="password" id="senha" class="form-control" placeholde="Senha" required >
+                
+                <button type="submit" class="btn btn-lg btn-primary btn-block" > Confirmar Cadastrar </button>
+        
 		 
     
 
