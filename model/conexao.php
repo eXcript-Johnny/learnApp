3e982fdef09f74be4f75267bf7269b23fc386607
OliveiraNadia <n.nadia.oliveira7@gmail.com>
2016-04-17 22:32:53 -0300

    Update conexao.php

--- model/conexao.php
+++ model/conexao.php
@@ -1,11 +1,33 @@
+<html>
+
+<head></head>
+
+
+<body>
 <?php
-function conexao ( $host, $user, $pass, $bd,$cn)
-{
-$host = 'localhost';
+
+/*$host = 'localhost';
 $user = 'root';
 $pass = '';
-$bd = '';
- 
-$cn = mysqli_connect($host, $user, $pass, $bd) or die ("falha na conexao");
-    
-}
+$bd = 'learnappbd';
+ */
+//$cn = mysqli_connect($host, $user, $pass, $bd) or die (mysql_error());
+ //$sql= mysql_select_bd($bd) or die (mysql_error());
+// echo "Voce esta conectado";
+
+if(mysqli_connect('localhost','root', '', 'learnappbd'))
+{
+	echo "conexao realizada ";
+		
+	
+} else 
+	
+	echo "erro na conexão";
+	
+	
+?>
+</body>
+
+
+</html>
+
