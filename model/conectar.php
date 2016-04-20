f605d8d623100441b73550436cd80242e304dbf5
OliveiraNadia <n.nadia.oliveira7@gmail.com>
2016-04-19 22:57:12 -0300

    Update conectar.php

--- model/conectar.php
+++ model/conectar.php
@@ -1,27 +1,44 @@
 <?php
-
-class Conectar{
-    public $db;
+ob_start();
+session_start();
+if(isset($_SESSION['email']) && (isset($_SESSION['senha']))){
+    header("Location: ../view/index.php"); exit();
+}
+   include("conexao.php");
+   
+if(isset($_POST['logar'])){
+    $usuario = trim(strip_tags($_POST['email']));
+    $senha = trim(strip_tags($_POST['senha']));
     
-    public function __construct() {
-        $db = new Database();
-        $this ->bd =$db ->instance();
-        $this ->sendContact();
-    }
-    public function sendContact(){
-        if($_POST){
-          try{
-             $query = $this ->$db -> prepare("INSERT INTO  usuarios (nome,email, telefone) values (:nadia,:nadia@gmail.com, :telefone)");
-             $query ->bindValue(":nome",$_POST['nome'],PDO::PARAM_STR);
-             $query ->bindValue(":email",$_POST['email'],PDO::PARAM_STR);
-             $query->execute();
-             echo"enviado com sucesso!";
-          } catch (PDOException $e) {
-              echo"Problema";
+    $select = "SELECT * from usuarios WHERE email=:usuario AND senha=:senha";
+  
+    try{
+        $result = $conexao->prepare($select);
+        $result ->bindParam(':email', $email, PDO::PARAM_STR);
+        $result ->bindParam(':senha',$senha,PDO::PARAM_STR);
+        $result->execute();
+        $cont = $result ->rowCount();
+        if($cont>0){
+            $email =['email'];
+            $senha =['senha'];
+            $_SESSION['email'] = $email;
+            $_SESSION['senha'] =$senha;
+            
+            echo '<div class = "alert alert-success">
+                 <button type="button" class="close" data-dismiss="alert"> x </button>
+                 <strong> logado com sucesso</strong> Redirecionando para o sistema.
+                 </div>';
+            header("Refresh:5, index.php");
+        }else{
+            echo 'os dados digitados estão incorretos';
+            
+        }
+    } catch (Exception $e) {
+        echo $e;
 
-          }  
-         
-        } 
     }
+    
+}
+
     
     ?>
