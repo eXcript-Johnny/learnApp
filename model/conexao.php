c8beeef83466d3c049fad2e63057619cfec3aa49
OliveiraNadia <n.nadia.oliveira7@gmail.com>
2016-04-20 00:59:28 -0300

    Update conexao.php

--- model/conexao.php
+++ model/conexao.php
@@ -1,52 +1,15 @@
 
 <?php
-class Database
-{
- 
-    protected static $db;
-
-
-    private function __construct()
-    {
-     
-        $db_host = "localhost";
-        $db_nome = "learnappbd";
-        $db_usuario = "root";
-        $db_senha = "";
-       
-
-        
-        try
-        {
-            
-            self::$db = new PDO("$db_driver:host=$db_host; dbname=$db_nome", $db_usuario, $db_senha);
-            
-            self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
-            self::$db->exec('SET NAMES utf8');
-        }
-		
-        catch (PDOException $e)
-        {
-		    echo $e->getMessage ();
-}
-            
-        }
-		
-    }
-
+<?php
+class Conexao{
     
-         function conexao()
-    {
-       
-        if (!self::$db)
-        {
-            new Database();
-			
-        }
-
-       
-    }
+	function conectar{
+		
+		
+	}
+	
+	
+}    
 
 
 ?>
