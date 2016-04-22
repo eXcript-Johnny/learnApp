71fc650b7d09bc9bcd3d3c7e091c86c0310cca80
OliveiraNadia <n.nadia.oliveira7@gmail.com>
2016-04-22 13:55:52 -0300

    Update conexao.php

--- model/conexao.php
+++ model/conexao.php
@@ -5,11 +5,13 @@ class Conexao{
         const user = "root";
 	const password = "";
         const base = "learnappbd";
+        
+        $pdo = new PDO('mysql:host=$host;dbname=$base',user,password);
+		$pdo = setAttribute(PDO::ATTR_ERRNOME, PDO::ARR_EXPECTION);
     
 	function conectar{
 
-		$pdo = new PDO('mysql:host=$host;dbname=$base',user,password);
-		$pdo = setAttribute(PDO::ATTR_ERRNOME, PDO::ARR_EXPECTION);
+		
 	}
 	
 	
