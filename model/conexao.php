45c85b3f924450dac555a2ab1cd6b915ebc12fbc
OliveiraNadia <n.nadia.oliveira7@gmail.com>
2016-04-22 22:18:24 -0300

    Update conexao.php

--- model/conexao.php
+++ model/conexao.php
@@ -10,6 +10,12 @@ class Conexao{
     
     
 	function conectar(){
+		
+	self::host; 
+	self::user;
+	self::password;
+	self::base;
+	
 	
 	$pdo = new PDO('mysql:host=$host;dbname=$base',$user,$password);
 	$pdo = setAttribute(PDO::ATTR_ERRNOME, PDO::ARR_EXPECTION);
