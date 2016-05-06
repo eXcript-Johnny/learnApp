b4b9fb91fe8be439a39d1ab4886917208889da5a
Letícia Barbosa <leticia@itsense.com.br>
2016-05-06 01:35:38 -0300

    estrutura

--- model/conexao.php
+++ model/conexao.php
@@ -1,31 +1,15 @@
 <?php
 
 class Conexao{
-	const  host = "localhost";
-	const  user = "root";
+	const  host     = "localhost";
+	const  user     = "root";
 	const  password = "";
-	const  base = "learnappbd";
-	
+	const  base     = "learnappbd";
 	
     function conectar(){
-	
 	$pdo = new PDO("msql: host=".self ::host.";dbname=" .self::base."; charset =utf8", self::user, self::password);
         $pdo = setAttribute(PDO::ATTR_ERRNOME, PDO::ARR_EXPECTION);
-	
-	
-	$pdo = new PDO('mysql:host=host;dbname=base,user,password');
-	$pdo = setAttribute(PDO::ATTR_ERRNOME, PDO::ARR_EXPECTION);
-	self::host . self::user . self::password .self::base ;
-	    return $pdo;
-	}
+	return $pdo;
+    }
 		
-	}
-	
-   
-		
-		
-	
-    
-
-
-?>
+}
--- model/usuario.php
+++ model/usuario.php
@@ -1,17 +1,22 @@
 <?php
-
-// incluir a class conexao com o include
-include_once 'conexao.php';
-
 class usuario extends Conexao{
     public $conexao;
-    
-    
+    public $usuario;
+    public $senha;
     
     function __construct(){
         $this->conexao = Conexao::conectar();
     }
     
+    
+    public function selectUsuario(){
+        $query        = "montar a query";
+        $consultation = $this->conexao->prepare($query);
+        $consultation->bindValue(":ID_cliente", $this->ID_cliente);
+        $consultation->execute();
+
+        return $consultation->fetchObject();
+    }
 }
 
 ?>
