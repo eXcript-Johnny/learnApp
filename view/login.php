f3e0a69600c5f726ec3b1edc30ab90fbaa761f71
OliveiraNadia <n.nadia.oliveira7@gmail.com>
2016-05-10 13:38:36 -0300

    Update login.php

--- view/login.php
+++ view/login.php
@@ -4,14 +4,37 @@
         <meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title> LearnApp Login </title>
-        <link rel="stylesheet" href="../view/css/bootstrap.css">
-	<link rel="stylesheet" href="../view/css/estilo.css">
-	<link rel="stylesheet" href="../view/css/bootstrap.min.css">
-        <script type="text/javascript" src="view/js/jquery-1.12.3.js"></script>
-        <script type="text/javascript" src="view/js/login.js"></script>
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/estilo.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/navBar.css">
+        <script type="text/javascript" src="js/jquery-1.12.3.js"></script>
+        <script type="text/javascript" src="js/login.js"></script>
+        <script type="text/javascript" src="js/cadastro.js"></script>
     </head>
     
     <body>
+        
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Learnaap</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+            <a class="navbar-brand" href="login.php">Learnapp - Sistema Inovador de Estudo </a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            
+          </ul>
+          
+        </div>
+      </div>
+    </nav>
+
         <div class="container form-login">   
             
                     <h2 class="form-login-heading"> Entre com o usuário</h2>
@@ -21,11 +44,11 @@
                     <input type="password" id="senha" class="form-control" placeholde="Senha">
                     <div class="checkbox">
                             <label>
-                            <input type="checkbox" value= "esqueci"> Esqueci a senha
+                                <a href="#"> Esqueci a senha</a>
                             </label>
                             </div>
                             <button type="submit" class="btn btn-lg btn-primary btn-block" onclick="evento.validar()"> Entrar </button>
-                            <button type="button" class="btn btn-lg btn-primary btn-block"> Cadastrar-se</button>
+                            <button type="button" class="btn btn-lg btn-primary btn-block" onclick="jsCadastrar()" > Cadastrar-se</button>
            
 	</div>
     </body>
